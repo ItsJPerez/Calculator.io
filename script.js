b64605788616e65eb081c9ece7b76665e97642da
JDevelops <99771082+JDevelopss@users.noreply.github.com>
2022-11-26 15:09:34 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+ const result = document.getElementById('input-text');
+
+ let calculate=(number)=>{
+    result.value+=number;
+ }
+
+ let Result =()=>{
+    try{
+        result.value=eval(result.value)
+    }
+    catch(err){
+        alert("Enter a valid operation");
+    }
+ }
+
+ function clr() {
+    result.value= " ";
+ }
+
+ function del() {
+    result.value= result.value.slice(0,-2);
+ }
